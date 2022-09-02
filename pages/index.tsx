f23b2a086e4c6883fbac0e1b6b5abf533495eb26
Joseph Boscan <joeypc.py@gmail.com>
2022-09-02 18:58:40 -0400

    Adding i18n to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import { Layout } from '@/components/Layout';
 import { Sidebar } from '@/components/Sidebar';
 import type { NextPage } from 'next';
+import { useTranslation, useLanguageQuery, LanguageSwitcher } from 'next-export-i18n';
 
 const Home: NextPage = () => {
+  const { t } = useTranslation();
+
   return (
     <Layout>
       <Sidebar />
+      <div style={{ fontSize: 30, marginLeft: 100 }}>
+        <LanguageSwitcher lang="es">es</LanguageSwitcher> | <LanguageSwitcher lang="en">en</LanguageSwitcher>
+        <h1>{t('index.test')}</h1>
+      </div>
     </Layout>
   );
 };
 
 export default Home;
+
